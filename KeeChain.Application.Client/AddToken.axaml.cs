26928f2d3649be87ac7a9f6ffbf4bd332906add3
Takhir Latypov <cregennandev@gmail.com>
2024-05-03 22:50:32 +0300

    "Клиент"

---
+++ KeeChain.Application.Client/AddToken.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KeeChain.Application.Client;
+
+public partial class AddToken : Window
+{
+    public AddToken()
+    {
+        InitializeComponent();
+    }
+}
---
+++ KeeChain.Application.Client/MainWindow.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KeeChain.Application.Client;
+
+using System;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        var current = Random.Shared.Next(0, 100);
+        
+        Random1.Text = Random.Shared.Next(100000, 999999).ToString();
+        Random2.Text = Random.Shared.Next(100000, 999999).ToString();
+        Random3.Text = Random.Shared.Next(100000, 999999).ToString();
+        Random4.Text = Random.Shared.Next(100000, 999999).ToString();
+        Bar1.Value = current;
+        Bar2.Value = current;
+        Bar3.Value = current;
+        Bar4.Value = current;
+        
+        AddBtn.Click += (_, _) => new AddToken().Show(this);
+    }
+}
