4e7e4f25b9f9730a3cdbcf61692b6f166fa2b7d0
Takhir Latypov <cregennandev@gmail.com>
2024-05-28 00:12:27 +0300

    Добавлен забытый запрос

---
+++ KeeChain.Warlin/Requests/UnlockRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace KeeChain.Warlin.Requests
+{
+    using Interfaces;
+    using Responses;
+
+    public class UnlockRequest : IWarlinRequestable<UnlockRequest, AckResponse>
+    {
+        public string IdentifyingToken => "UNLOCK";
+
+        public readonly string MasterPassword;
+
+        public UnlockRequest(string masterPassword) => MasterPassword = masterPassword;
+
+        public IEnumerable<string>? ToTokens() => [MasterPassword];
+    }
+}
